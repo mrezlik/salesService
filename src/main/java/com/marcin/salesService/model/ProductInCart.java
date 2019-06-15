7117b6285817f7c4ec28b262351fcebdea8a48ec
mrezlik <mrezlik@gmail.com>
2019-06-15 13:19:28 +0200

    Create product in cart model

--- src/main/java/com/marcin/salesService/model/ProductInCart.java
+++ src/main/java/com/marcin/salesService/model/ProductInCart.java
@@ -1,17 +1,16 @@
 package com.marcin.salesService.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductInCart {
 
     private Product product;
 
     private int quantity;
 
-    public ProductInCart(Product product, int productQuantity) {
-        this.product = product;
-
-        this.quantity = productQuantity;
-    }
 }
