dcc99c8ff4f1d7aa5468580987de19dcf687b3e1
mrezlik <mrezlik@gmail.com>
2019-06-15 13:28:49 +0200

    Remove unused method in product in warehouse repository

--- src/main/java/com/marcin/salesService/repository/ProductInWarehouseRepository.java
+++ src/main/java/com/marcin/salesService/repository/ProductInWarehouseRepository.java
@@ -10,6 +10,5 @@ import java.util.List;
 public interface ProductInWarehouseRepository extends JpaRepository<ProductInWarehouse, Integer> {
 
     List<ProductInWarehouse> findAll();
-    ProductInWarehouse findById(int id);
 
 }
