40b5eea9677b85751c283bec38cd1f22addbdbdd
mrezlik <mrezlik@gmail.com>
2019-06-15 13:28:02 +0200

    Add javadoc and remove unused method

--- src/main/java/com/marcin/salesService/service/WarehouseService.java
+++ src/main/java/com/marcin/salesService/service/WarehouseService.java
@@ -1,5 +1,6 @@
 package com.marcin.salesService.service;
 
+import com.marcin.salesService.exception.ProductNotFoundException;
 import com.marcin.salesService.model.ProductInWarehouse;
 import com.marcin.salesService.model.Warehouse;
 import com.marcin.salesService.repository.ProductInWarehouseRepository;
@@ -19,30 +20,26 @@ public class WarehouseService {
     public WarehouseService(ProductInWarehouseRepository productInWarehouseRepository) {
         this.productInWarehouseRepository = productInWarehouseRepository;
         warehouse = new Warehouse();
-        warehouse.setProductInWarehouse(productInWarehouseRepository.findAll());
+        warehouse.setProductInWarehouse(this.productInWarehouseRepository.findAll());
     }
 
     public List<ProductInWarehouse> getProductsInWarehouse() {
         return warehouse.getProductInWarehouse();
     }
 
+    /**
+     * This method get all product from warehouse and filter it by given id
+     * If in warehouse is no product with this id throw ProductNotFoundException
+     * @throws ProductNotFoundException
+     * @param productID
+     * @return ProductInWarehouse
+     */
     public ProductInWarehouse getProductInWarehouseById(int productID) {
-        return warehouse.getProductInWarehouse().stream().filter(productInWarehouse -> productInWarehouse.getProduct().getId() == productID).findFirst().get();
+        return warehouse.getProductInWarehouse()
+                            .stream()
+                            .filter(productInWarehouse -> productInWarehouse.getProduct().getId() == productID)
+                            .findFirst()
+                            .orElseThrow(ProductNotFoundException::new);
     }
 
-    void substractFromWarehouse(ProductInWarehouse productInWarehouse, int quantity){
-
-        int leftInWarehouse = productInWarehouse.getQuantity() - quantity;
-
-        productInWarehouse.setQuantity(leftInWarehouse);
-
-    }
-
-    public void addToWarehouse(ProductInWarehouse productInWarehouse, int quantity) {
-
-        int leftInWarehouse = productInWarehouse.getQuantity() + quantity;
-
-        productInWarehouse.setQuantity(leftInWarehouse);
-
-    }
 }
